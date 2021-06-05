4caf19bb68127829ce6add9a2d43391ea0140d73
subcorporeal <subcorporeal@gmail.com>
2021-06-05 00:26:43 -0500

    Crud updated

--- 1_Ciclo1_fundamentos/Unidad_5/Clase_14/crud/agenda.py
+++ 1_Ciclo1_fundamentos/Unidad_5/Clase_14/crud/agenda.py
@@ -1,12 +1,16 @@
 import pandas as pd
+from pandas.core.frame import DataFrame
+
+
+def get_data_frame() -> DataFrame:
+    return pd.read_csv('agenda.csv')
 
 
 def read_agenda():
     print('-------------* Leer agenda *-------------\n')
     # leer agenda con pandas ||
     # lo organiza en un formato
-    rd = pd.read_csv('agenda.csv')
-    print(rd, '\n \n')
+    print(get_data_frame(), '\n \n')
     # Sale de la agenda
     exit()
 
@@ -31,12 +35,25 @@ def create_contact():
     fr.close()
 
 
+def update_contact():
+    print('Seleccione el contacto que desea actualizar: ')
+    print(get_data_frame()['name'])
+    selection = int(input('Selecciona el numero correspondiente: '))
+    new_df = get_data_frame()
+    new_df.loc[selection, 'cel'] = int(input('Nuevo número: '))
+    print('Contacto actualizado: ')
+    print(new_df.loc[selection, ['cel', 'name']])
+    new_df.to_csv('agenda.csv', index=False)
+    # print(get_data_frame().loc[selection, 'cel'])
+
+
+# ---------- Mi solución-----------------
 def search_by_name():
     # se pide el nombre y se convierte en minúsculas
     search = input('Digite el nombre: ').lower()
 
     # leemos los datos y pasamos la columna name a los índices
-    agenda_data = pd.read_csv('agenda.csv')
+    agenda_data = get_data_frame()
     agenda_name_indexed = agenda_data.set_index('name')
 
     # con un map convertimos todos los nombres de la lista en minúsculas
@@ -46,7 +63,8 @@ def search_by_name():
     ocurrences = list(filter(lambda name: name.find(
         search) >= 0, names))
 
-    # imprimimos convirtiendo cada inicial de la lista en una capital o mayuscula
+    # imprimimos convirtiendo cada inicial de la lista
+    # en una capital o mayuscula
     # para que pueda encontrarlo en el dataframe a través de loc
     if ocurrences != []:
         print(
@@ -68,6 +86,7 @@ def show_menu():
             1- Crear Contacto
             2- Leer agenda
             3- Buscar por nombre
+            4- Actualizar número celular
             9- Salir
 
         """
@@ -80,6 +99,8 @@ def show_menu():
         read_agenda()
     elif option == '3':
         search_by_name()
+    elif option == '4':
+        update_contact()
     elif option == '9':
         exit()
 
@@ -93,6 +114,6 @@ def run():
 if __name__ == '__main__':
     # crear un dataframe
     # df = pd.DataFrame(columns=['name', 'cel', 'email'])
-    # # crear un archivo .csv desde el dataframe
+    # crear un archivo .csv desde el dataframe
     # df.to_csv('agenda.csv', index=False)
     run()
