89b2c2da7931900d95ac113c9da77cae4820b5cf
subcorporeal <subcorporeal@gmail.com>
2021-07-12 23:28:36 -0500

    Wallet updated

--- Ciclo_2_java/Unidad_1/Clase_1/wallet/src/model/Wallet.java
+++ Ciclo_2_java/Unidad_1/Clase_1/wallet/src/model/Wallet.java
@@ -1,9 +1,11 @@
 package model;
 
+import java.util.Scanner;
+
 public class Wallet {
 
     // Create the constant for the wallet limit
-    public static final int WALLET_LIMIT = 5000000;
+    public static final int WALLET_LIMIT = 500000;
     /**
      * wallet balance
      */
@@ -28,6 +30,31 @@ public class Wallet {
         }
         balance += value;
         return "Operation succeed! your new balance is " + balance;
+    }
 
+    // My solution
+    public String withdrawBalance(int withdraw) {
+        // Validate if have balance
+        if (balance == 0) {
+            return "Sorry, you have insufficient funds";
+        } else if (withdraw <= balance && withdraw >= 10000) {
+            // Validate if withdraw is grater than balance and meets the minimun of 10000
+            balance -= withdraw;// Subtract the witdraw from the balance
+            return "Operation succeed!, \n you have made a withdrawal for: " + withdraw + "\n Your balance now is: "
+                    + balance; // return the succed message
+        }
+
+        return "Sorry, you are only allowed to withdraw a minimum of 10000 , please try again.";
+    }
+
+    // The lesson solution
+    public String getMoney(int value){
+        if(value > balance){
+            int tempBalance = balance;
+            
+            return "Sorry, you have insufficient funds";
+        }
+        balance -= value;
+        return "Succesful balance withdrawal "
     }
 }
--- Ciclo_2_java/Unidad_1/Clase_1/wallet/src/view/View.java
+++ Ciclo_2_java/Unidad_1/Clase_1/wallet/src/view/View.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.Scanner;
+
 import model.Wallet;
 
 public class View {
@@ -8,7 +10,17 @@ public class View {
         Wallet wallet1 = new Wallet(true);
         Wallet wallet2 = new Wallet(false);
 
-        wallet1.getBalance();
-        wallet2.getBalance();
+        String balance1 = wallet1.setBalance(600000);
+        System.out.println(balance1 + "\n" + wallet1.getBalance());
+        String balance2 = wallet2.setBalance(600000);
+        System.out.println(balance2 + "\n" + wallet2.getBalance());
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("New withdraw for wallet 2: ");
+        System.out.println("How much would you like to withdraw?: ");
+        int withdraw2 = sc.nextInt();
+
+        System.out.println(wallet2.withdrawBalance(withdraw2));
+        sc.close();
     }
 }
